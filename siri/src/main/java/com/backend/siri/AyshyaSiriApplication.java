35862a86f2e53b03cefc88d32ac27156ad8738a9
KushwahaS <KushwahaS@INBANGNBDELL18.checkpt.com>
2023-02-24 12:48:11 +0530

    Packages created

---
+++ siri/src/main/java/com/backend/siri/AyshyaSiriApplication.java
@@ -0,0 +1,13 @@
+package com.backend.siri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyshyaSiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyshyaSiriApplication.class, args);
+	}
+
+}
---
+++ siri/src/test/java/com/backend/siri/AyshyaSiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.siri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyshyaSiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
